7202c7ef072e89ca4d237eec5e286171344f43f3
Kazuki <wa_1023@yahoo.co.jp>
2017-06-22 01:34:24 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CallComyu::Application.config.secret_key_base = 'd11b6c2d85ac8c8adcd828f70b2f8d5885cdf92fb478b7fa1f94e828c7aced49ff8c40469a2d0d5c1d7c098345e2574eb1e90c9cd80ec8f38f91b2d02e0b10dd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CallComyu::Application.config.session_store :cookie_store, key: '_call_comyu_session'
